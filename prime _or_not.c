08fd4a9cf5ad786f2748bdb38241f45ca9f97ce4
snehadaniyal05-cyber <snehadaniyal05@gmail.com>
2025-11-19 22:38:03 +0530

    Add files via upload

---
+++ prime _or_not.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int num,count=0;
+     printf("Enter a number: ");
+     scanf("%d",&num);
+
+     for(int i=1; i<=num; i++)
+     {
+         if(num%i==0){
+         count++;
+    
+        }
+    }
+    if(count==2){
+        printf("prime number");
+    }
+    else 
+        printf("not a prime number");
+  return 0;
+}
