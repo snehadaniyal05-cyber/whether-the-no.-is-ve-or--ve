8bb488c5bd02cde3ff05aebe6e0a4737103e8f7f
snehadaniyal05-cyber <snehadaniyal05@gmail.com>
2025-11-19 22:19:15 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    if(n%2==0)
+    printf("The number is even");
+    else
+    print("The number is odd");
+return 0;
+}
